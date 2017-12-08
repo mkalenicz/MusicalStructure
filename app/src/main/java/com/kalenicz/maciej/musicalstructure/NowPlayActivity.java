29f5906ead1c249ae81e4e13218efab76f346d5a
maciej <maciej.kalenicz@gmail.com>
2017-12-08 16:53:47 +0100

    play/pause button in NowPlayActivity

--- app/src/main/java/com/kalenicz/maciej/musicalstructure/NowPlayActivity.java
+++ app/src/main/java/com/kalenicz/maciej/musicalstructure/NowPlayActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class NowPlayActivity extends AppCompatActivity {
 private static final String TAG = "NowPlayActivity";
+    boolean isPlaying = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -17,7 +20,7 @@ private static final String TAG = "NowPlayActivity";
         Log.v(TAG, "NowPlayActivity works");
 
         Intent intent = getIntent();
-        String songName = intent.getStringExtra("song_name");
+        final String songName = intent.getStringExtra("song_name");
 //        String albumName = intent.getStringExtra("album_name");
         String artistName = intent.getStringExtra("artist_name");
         int albumImage = intent.getIntExtra("album_image", R.drawable.nirvana);
@@ -30,5 +33,25 @@ private static final String TAG = "NowPlayActivity";
 
         TextView artistNameTextView = findViewById(R.id.now_play_artist_name);
         artistNameTextView.setText(artistName);
+
+//        Toast.makeText(getApplicationContext(), "Now you are listen song", Toast.LENGTH_SHORT);
+        ImageView playbutton = findViewById(R.id.play_pause_button);
+
+        playbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isPlaying){
+                    ImageView pausebutton = findViewById(R.id.play_pause_button);
+                    pausebutton.setImageResource(R.drawable.ic_pause_circle_outline_black_24dp);
+                    Toast toast = Toast.makeText(getApplicationContext(), "Now you are listening song: " + songName, Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    ImageView playbutton = findViewById(R.id.play_pause_button);
+                    playbutton.setImageResource(R.drawable.ic_play_circle_outline_black_24dp);
+                }
+                isPlaying = !isPlaying;
+
+            }
+        });
     }
 }
